4d342b4179672f155905377d95701e8208683b8b
binhchay1 <binhchay1@gmail.com>
2026-06-01 01:57:23 +0700

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title inertia>{{ config('app.name', 'ERM SaleOps') }}</title>
+        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
         @inertiaHead
